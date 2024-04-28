62881ce07e21baea5ed172cf5433e3d4959b512f
Mathilde NADEAU <mathilde.nadeau@hotmail.fr>
2024-04-28 16:46:33 +0200

    fix affichage vache list

--- resources/js/components/vaches/VachesList.vue
+++ resources/js/components/vaches/VachesList.vue
@@ -5,10 +5,10 @@ v-for="vache in vaches"
     <div class="bg-gradient-to-r from-yellow-50 to-yellow-300 my-2 p-5 rounded-md shadow-md">
         <p class="font-sans text-xl hover:italic">{{ vache.name }}</p>
         <p>{{ vache.description }}</p>
-        <p>{{ vache.price }}</p>
-        <p>{{ vache.is_sold }}</p>
-        <p>{{ vache.category.name }}</p>
-        <p>{{ vache.race.name }}</p>
+        <p>Prix: {{ vache.price }} €</p>
+        <p>{{ vache.is_sold ? 'Vendu' : 'En vente' }}</p>
+        <p>Type: {{ vache.category.name }}</p>
+        <p>Race: {{ vache.race.name }}</p>
     </div>
 </template>
 </template>
