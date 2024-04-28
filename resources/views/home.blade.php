4e34c954ebb3b93fff6fd2ccf026bd98c1ac3e07
Mathilde NADEAU <mathilde.nadeau@hotmail.fr>
2024-04-28 23:05:37 +0200

    misc; style fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="flex flex-col md:flex-row justify-center items-center">
         <div class="md:w-1/2 p-5">
             <h2 class="text-3xl font-bold font-mono my-5 text-center md:text-left">Bienvenue à la ferme fictive</h2>
-            <a class="bg-gradient-to-r from-yellow-50 to-yellow-300 p-2 rounded-md text-sm hover:text-blue-500 my-5 text-center md:text-left text-xl" href="/vaches">Voir les animaux</a>
+            <a class="bg-gradient-to-r from-yellow-50 to-yellow-300 p-2 rounded-md text-sm hover:text-blue-500 my-5 text-center md:text-left text-xl font-semibold" href="/vaches">Voir les animaux</a>
         </div>
         <div class="md:w-1/2 p-5">
             <img class="h-auto w-full max-w-lg transition-all duration-300 rounded-lg cursor-pointer filter grayscale hover:grayscale-0" src="./home.jpg" alt="">
