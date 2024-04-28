e1126b4e8cf7a24e00cea1845dac224918059583
Mathilde NADEAU <mathilde.nadeau@hotmail.fr>
2024-04-28 18:24:18 +0200

    fix style

--- resources/views/vaches/index.blade.php
+++ resources/views/vaches/index.blade.php
@@ -1,8 +1,9 @@
 @extends('app')
 
 @section('content')
-<h2 class="text-3xl font-bold underline my-3">Liste des vaches</h2>
+<h2 class="text-3xl font-bold underline my-3">Liste du bétail</h2>
 <a class="bg-gradient-to-r from-yellow-50 to-yellow-300 p-2 rounded-md text-sm text-blue-500 hover:text-blue-800 my-5" href="{{ route('vaches.create')}}">Ajouter une vache</a>
+<div class="p-5">
+    <vaches-list /></div>
 
-<vaches-list />
 @endsection
